63935fd1a7b1314b8088933c236710f38b2e84a7
Marcelo <marcaldo@gmail.com>
2024-03-09 15:02:21 -0600

    first commit

--- src/app.py
+++ src/app.py
@@ -5,8 +5,6 @@ from io import BytesIO
 
 def get_image():
     try:
-        error_label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
-
         # Make a GET request to the API endpoint that returns the image
         response = requests.get("https://images.stockcake.com/public/0/9/6/096537ac-2343-4187-b150-e2fa00120af8/autumn-lake-view-stockcake.jpg")
         # Check if the request was successful (status code 200)
@@ -23,20 +21,15 @@ def get_image():
             # Keep a reference to the photo to prevent garbage collection
             canvas.image = photo
             
-            # Create a label on top of the canvas
-            # label = tk.Label(root, text="", bg="white", highlightthickness=0)
-            # label.place(relx=0.5, rely=0.1, anchor=tk.CENTER)  # Position the label in the center of the screen, 10% from the top
-
         else:
             # Display an error message if the request was unsuccessful
-            error_label.config(text=f"Failed to retrieve image. Status code: {response.status_code}")
-            # Position the error label over the canvas
-            error_label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+            error_label = tk.Label(root, fg="red", text=f"Failed to retrieve image. Status code: {response.status_code}")
+            error_label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)  # Position the error label over the canvas
 
     except Exception as e:
         # Display an error message if an exception occurs
-        error_label.config(text=f"An error occurred: {str(e)}")
-        error_label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+        error_label = tk.Label(root, fg="red", text=f"An error occurred: {str(e)}")
+        error_label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)  # Position the error label over the canvas
 
 # Create the Tkinter window
 root = tk.Tk()
@@ -51,17 +44,11 @@ root.attributes("-fullscreen", True)
 root.attributes("-fullscreen", True)
 root.configure(bg="black")
 
-
 # Create a canvas to display the image
 canvas = tk.Canvas(root, borderwidth=0, highlightthickness=0)
 canvas.pack(fill=tk.BOTH, expand=True)
 
-# Create a label to display error messages
-error_label = tk.Label(root, fg="black")
-# Position the error label over the canvas
-error_label.place_forget()
-error_label.pack()
-
+root.config(cursor="none")
 
 # Load the image automatically when the application starts
 get_image()
