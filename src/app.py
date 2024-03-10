0d50e93c8ab2b5918fb63ecfe054b6a0cf14d479
Marcelo <marcaldo@gmail.com>
2024-03-10 13:09:32 -0500

    Getting files directory

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import os
 from image_local_loader import ImageLocalLoader
 from video_player import VideoPlayer
 
@@ -8,17 +9,25 @@ root.title("Full Screen Image Viewer")
 root.attributes("-fullscreen", True)
 root.config(cursor="none")
 
-# fileName = "C:/Users/marca/source/repos/TimeReflect/Pictures/dragon.mp4"
-fileName = "C:/Users/marca/source/repos/TimeReflect/Pictures/vertical.jpg"
+albumsPath = "C:/Users/marca/source/repos/TimeReflect/Albums"
+albumName = "album1"
+fileName = "dragon.MP4"
+
+# script_dir = os.path.dirname(__file__)
+# two_levels_up_dir = os.path.dirname(script_dir)
 
 isVideo = fileName.lower().endswith(".mp4")
 
+fileFullName = "/".join([albumsPath, albumName, fileName])
+
 if isVideo:
-    video_player = VideoPlayer(root, fileName)
+    video_player = VideoPlayer(root, fileFullName)
 else:
     image_loader = ImageLocalLoader(root)
-    image_loader.load_image(fileName)
+    image_loader.load_image(fileFullName)
 
 
 # Run the Tkinter event loop
 root.mainloop()
+
+
