5afbea8b88fda1d997fce55b6e1b76d3b9a1f9a7
Marcelo <marcaldo@gmail.com>
2024-03-10 11:46:51 -0500

    fullscreen video

--- src/app.py
+++ src/app.py
@@ -8,24 +8,24 @@ root.title("Full Screen Image Viewer")
 
 # Configure the window to be full screen
 root.attributes("-fullscreen", True)
-root.configure(bg="black")  # Set background color to black to match the image background
-
-# Set border width to 0 and remove padding
-root.attributes("-fullscreen", True)
-root.attributes("-fullscreen", True)
-root.configure(bg="black")
-
-# Create an instance of ImageLoader
-image_loader = ImageLocalLoader(root)
-
 root.config(cursor="none")
+# root.configure(bg="black")  # Set background color to black to match the image background
+
+# # Set border width to 0 and remove padding
+# root.attributes("-fullscreen", True)
+# root.attributes("-fullscreen", True)
+# root.configure(bg="black")
+
+# # Create an instance of ImageLoader
+# image_loader = ImageLocalLoader(root)
+
 
 # Load the image
 # image_loader.load_image("https://images.stockcake.com/public/0/9/6/096537ac-2343-4187-b150-e2fa00120af8/autumn-lake-view-stockcake.jpg")
 # image_loader.load_image("C:/Users/marca/source/repos/TimeReflect/Pictures/20180620_152359.jpg")
 #image_loader.load_image("C:/Users/marca/source/repos/TimeReflect/Pictures/vertical.jpg")
 
-video_player = VideoPlayer(root, "C:/Users/marca/source/repos/TimeReflect/Pictures/summerSnow.mp4")
+video_player = VideoPlayer(root, "C:/Users/marca/source/repos/TimeReflect/Pictures/fire.mp4")
 
 # Run the Tkinter event loop
 root.mainloop()
